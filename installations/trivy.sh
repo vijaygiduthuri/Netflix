839ef3b7a373afb7e9973a3dbe5b0ff8f97f0378
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-08-19 15:33:25 +0530

    trivy.sh

---
+++ installations/trivy.sh
@@ -0,0 +1,5 @@
+sudo apt-get install wget apt-transport-https gnupg
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
